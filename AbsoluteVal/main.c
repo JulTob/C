721b70371c4b518ffd5667797a347725becc4135
Julio <JulioToboso@gmail.com>
2020-06-06 15:32:21 +0200

    Create main.c

---
+++ AbsoluteVal/main.c
@@ -0,0 +1,15 @@
+// if
+
+// Calculate the absolute value of integer
+#include <stdio.h>
+
+int main(void){
+   int number;
+   printf("Type in your number: ");
+   scanf("%i", &number);
+   if(number<0)
+      number = - number;
+   printf("The absolute value is %i \n");
+   return 0;
+   };
+   
