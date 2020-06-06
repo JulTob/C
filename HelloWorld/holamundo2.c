07f13397c4df1d7f6792329ebc87bbf5a27e2c63
Julio <JulioToboso@gmail.com>
2020-06-06 12:01:00 +0200

    Create holamundo2.c

---
+++ HelloWorld/holamundo2.c
@@ -0,0 +1,17 @@
+// Hola mundo
+
+#include <stdio.h>
+  /// Inclusion de libreria estandar
+
+// Funcion principal
+main(){
+  // Codigo del algoritmo principal
+  printf("hola, ");
+  printf("mundo.");
+  printf("\n");
+  }
+  
+/* Ejecucion:
+>> hola,mundo. [nueva linea]
+
+*/
