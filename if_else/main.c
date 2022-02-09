ba6245b432f0a110c758026891c3dbc7c26512ff
Julio <JulioToboso@gmail.com>
2022-02-09 11:35:10 +0100

    Create main.c

---
+++ if_else/main.c
@@ -0,0 +1,15 @@
+//  if-else
+//  Determine if a number is 
+//  even or odd
+#include <stdio.h>
+int main(void){
+   int number_to_test, remainder;
+  printf("Enter your number to be tested: ");
+  scanf("%i", number_to_test);
+  remainder = number_to_test % 2;
+  if(remainder == 0)
+    printf("The number is even. \n";
+  else
+    printf("The number is odd. \n";
+  return 0;
+  }
